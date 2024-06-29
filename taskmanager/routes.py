cc4bbd2be13013d42e177cb8fbbb425b691f9ebc
Niall Caplis <niallcaplis@gmail.com>
2024-06-29 19:36:25 +0000

    Create form for adding categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -6,3 +6,21 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        # Our form field's name matches our model key
+        category = Category(category_name=request.form.get("category_name"))
+        # Add inputted category to database's "category" log
+        db.session.add(category)
+        db.session.commit()
+        # Redirect the user back to the categories page
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
