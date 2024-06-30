eb98d0190b397efae331f33930d4ddaaccdc7e36
Niall Caplis <niallcaplis@gmail.com>
2024-06-30 19:45:24 +0000

    Create form to add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Initialize sidenav
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // Initialize datepicker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    });
+
+    // Initialize dropdown
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
